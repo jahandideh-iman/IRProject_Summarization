7d82760faebb4b230b2c31a9121fe9848172ba43
jahandideh-iman <jahandideh_iman@yahoo.com>
2016-01-14 10:26:37 +0330

    Adds lambda input parameter to application. Adds a method to remove tweets metadata

--- Converter/main.cpp
+++ Converter/main.cpp
@@ -1,6 +1,8 @@
 #include <string>
 #include <fstream>
 #include <iostream>
+#include <sstream>
+#include <algorithm>
 
 using namespace std;
 
@@ -21,21 +23,33 @@ void convertToLemurFormat(ifstream &in, ofstream &out) {
 void removeTweetMetaData(ifstream &in, ofstream &out)
 {
 	string line;
-	while (std::getline(in, line)) {
-		//TODO: write your code here.
+	while ( std::getline(in, line) ) {
+		stringstream ss(line);
+		string tmp;
+		ss >> tmp;
+		ss >> tmp;
+		ss >> tmp;
+		std::transform(tmp.begin(), tmp.end(), tmp.begin(), ::tolower);
+		if(tmp != "rt" && tmp != "v") {
+			out << tmp;
+		}
+		while(ss >> tmp)
+			out << " "<< tmp;
+		out<< endl;
 	}
 }
 
 int main(int argc, char* argv[]) 
 {
-	if(argc != 3)
+	if(argc != 4)
 	{
-		std::cerr << "Usage: " << argv[0] << " Input file path, Output file path" << std::endl;
+		std::cerr << "Usage: " << argv[0] << "Conversion Type, Input file path, Output file path" << std::endl;
 		return -1;
 	}
 
-	string inputPath = argv[1];
-	string outputPath = argv[2];
+	string conversionType = argv[1];
+	string inputPath = argv[2];
+	string outputPath = argv[3];
 
 	ifstream input(inputPath);
 	ofstream output(outputPath);
@@ -45,9 +59,11 @@ int main(int argc, char* argv[])
 		std::cerr << "Could Not open file" << std::endl;
 		return -1;
 	}
-
-	//convertToLemurFormat(input, output);
-	removeTweetMetaData(input, output);
+	
+	if(conversionType == "TRIM")
+		removeTweetMetaData(input, output);
+	else if(conversionType == "LEMUR")
+		convertToLemurFormat(input, output);
 
 	output.close();
 
